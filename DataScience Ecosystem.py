438298214b0f50da805357615a6b990bcba3a4c7
TarakKtari <137309201+TarakKtari@users.noreply.github.com>
2023-12-06 11:12:38 +0100

    Add files via upload

---
+++ DataScience Ecosystem.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Data Science Tools and Ecosystem
+# ## Objectives:
+# 
+# * List popular languages for Data Science
+# * Identify common data science libraries
+# * Explore data science tools
+# * Convert minutes to hours using Python
+# 
+
+# #### In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python: Python is a general-purpose language that is well-suited for data science because of its readability, ease of use, and large ecosystem of libraries for data manipulation, analysis, and visualization.
+# 
+# 2. R: R is a programming language specifically designed for statistical computing and graphics. It is widely used in data science for tasks such as statistical analysis, data modeling, and machine learning.
+# 
+# 3. SQL: SQL (Structured Query Language) is a language used to interact with databases. It is essential for data scientists to be able to query and manipulate data stored in databases.
+# 
+# 4. Julia: Julia is a relatively new programming language that is gaining popularity in the data science community. It is known for its speed, efficiency, and ability to handle large datasets.
+# 
+# 5. Java: Java is a versatile programming language that can be used for a wide variety of tasks, including data science. It is often used for developing large-scale data processing applications.
+# 
+# 6. Scala: Scala is a functional programming language that is gaining popularity in the data science community. It is known for its scalability, expressiveness, and ability to handle large datasets.
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. **NumPy:** NumPy is a fundamental library for scientific computing in Python. It provides efficient numerical operations on arrays, matrices, and other multidimensional data structures. NumPy is widely used for data manipulation, analysis, and visualization.
+# 
+# 2. **Pandas:** Pandas is a library for data analysis and manipulation in Python. It provides high-level data structures and functions for working with tabular data, such as DataFrames and Series. Pandas is widely used for data cleaning, wrangling, and exploratory data analysis.
+# 
+# 3. **Matplotlib:** Matplotlib is a comprehensive 2D plotting library for Python. It provides a variety of functions for creating charts, graphs, and other visualizations. Matplotlib is widely used for data visualization and exploration.
+# 
+# 4. **Scikit-learn:** Scikit-learn is a machine learning library for Python. It provides a wide range of supervised and unsupervised learning algorithms, including classification, regression, clustering, and dimensionality reduction. Scikit-learn is widely used for machine learning modeling and experimentation.
+# 
+# 5. **TensorFlow:** TensorFlow is an open-source end-to-end differentiable programming platform for machine learning. It is used for building and deploying a wide variety of models, including deep learning models. TensorFlow is widely used for deep learning and neural network applications.
+# 
+# 6. **PyTorch:** PyTorch is another open-source end-to-end differentiable programming platform for machine learning. It is similar to TensorFlow in its capabilities, but it offers a more Pythonic syntax and a more dynamic runtime. PyTorch is gaining popularity in the deep learning community.
+# 
+
+# | Data Science Tools |
+# |---|---|
+# | <a href="https://jupyter.org/">Jupyter Notebook</a> |
+# | <a href="https://rstudio.com/">RStudio</a> |
+# | <a href="https://spyder-ide.org/">Spyder</a> |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[1]:
+
+
+##This a simple arithmetic expression to mutiply then add integers.
+(3*4)+5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+hours = 200 / 60
+print(hours)
+
+
+# ## Author
+# 
+# Tarak
+
+# In[ ]:
+
+
+
+
